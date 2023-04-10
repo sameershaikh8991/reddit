a8b3407329f11151f2c0cd53dd0973fdd3e9075e
sameershaikh <43517756+sameershaikh8991@users.noreply.github.com>
2023-04-10 16:45:18 +0530

    new change

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.app.config;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +19,10 @@ import org.springframework.security.web.SecurityFilterChain;
 //
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private  final UserDetailsService userDetailsService;
 
 
 @Bean
@@ -30,7 +37,12 @@ public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws
                     .authenticated())
             .build();
 }
-//
+
+    @Bean
+    public AuthenticationManager authenticationManager( AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 @Bean
 PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
