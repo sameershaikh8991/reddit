c66cd5b558faeb7af138b076e62929de2a297057
sameershaikh <43517756+sameershaikh8991@users.noreply.github.com>
2023-04-10 16:37:31 +0530

    ASYNC MAIL

--- src/main/java/com/app/RadditApplication.java
+++ src/main/java/com/app/RadditApplication.java
@@ -2,8 +2,10 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RadditApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/app/service/MailService.java
+++ src/main/java/com/app/service/MailService.java
@@ -11,6 +11,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,11 +22,9 @@ public class MailService {
 
 
     private  final JavaMailSender mailSender;
-
     private  final  MailContentBuilder mailContentBuilder;
 
-
-
+    @Async
     void sendMail(NotificationEmail notificationEmail) throws Exception {
 
         MimeMessagePreparator messagePreparator = mimeMessage -> {
